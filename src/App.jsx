603f0a0b5eea810f902b7828b80100de91923cec
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-06-10 11:38:40 +0200

    importing vans and VanDetails

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
-/**
- * Challenge:
- * Bootstrap the VanLife project by creating the first 2 routes:
- * Home and About.
- * 
- * Also include the navbar that can link between the two routes.
- * For now, you'll either need to copy/paste the navbar code
- * to both Home and About pages, or you'll need to find a place
- * to put it where it can be shared between the two pages.
- * (Don't overthink this part - just do whatever is easiest for
- * you because we'll learn a better approach very soon)
- * 
- * Review challenge: do all the CSS yourself based on the design
- * linked in the slides.
- */
+import Vans from "../pages/Vans"
+import VanDetail from "../pages/VanDetail"
+import "./server"
 
 function App() {
 
-  return (
-    <h1>Start here</h1>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
