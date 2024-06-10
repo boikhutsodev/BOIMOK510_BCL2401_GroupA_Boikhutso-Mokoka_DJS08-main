bbee175260f7b3c8eca110af32ae81354d9a1838
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-06-10 11:59:52 +0200

    Saving import files

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Home from "../pages/Home"
-import About from "../pages/About"
-import Vans from "../pages/Vans/Vans"
-import VanDetail from "../pages/Vans/VanDetail"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Vans from "../pages/Vans/Vans";
+import VanDetail from "../pages/Vans/VanDetail";
 
-import Login from "../pages/Login"
-import Dashboard from "../pages/Host/Dashboard"
-import Income from "../pages/Host/Income"
-import Reviews from "../pages/Host/Reviews"
-import HostVans from "../pages/Host/HostVans"
-import HostVanDetail from "../pages/Host/HostVanDetail"
-import HostVanInfo from "../pages/Host/HostVanInfo"
-import HostVanPricing from "../pages/Host/HostVanPricing"
-import HostVanPhotos from "../pages/Host/HostVanPhotos"
-import NotFound from "../pages/NotFound"
-import Layout from "../components/Layout"
-import HostLayout from "../components/HostLayout"
-import AuthRequired from "../components/AuthRequired"
+import Login from "../pages/Login";
+import Dashboard from "../pages/Host/Dashboard";
+import Income from "../pages/Host/Income";
+import Reviews from "../pages/Host/Reviews";
+import HostVans from "../pages/Host/HostVans";
+import HostVanDetail from "../pages/Host/HostVanDetail";
+import HostVanInfo from "../pages/Host/HostVanInfo";
+import HostVanPricing from "../pages/Host/HostVanPricing";
+import HostVanPhotos from "../pages/Host/HostVanPhotos";
+import NotFound from "../pages/NotFound";
+import Layout from "../components/Layout";
+import HostLayout from "../components/HostLayout";
+import AuthRequired from "../components/AuthRequired";
 
-import "./server"
+import "./server";
 
 function App() {
   return (
@@ -29,10 +29,7 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="vans" element={<Vans />} />
           <Route path="vans/:id" element={<VanDetail />} />
-          <Route
-            path="login"
-            element={<Login />}
-          />
+          <Route path="login" element={<Login />} />
 
           <Route element={<AuthRequired />}>
             <Route path="host" element={<HostLayout />}>
@@ -52,7 +49,7 @@ function App() {
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
